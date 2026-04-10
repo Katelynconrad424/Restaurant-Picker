f53409319612847d93ca327430558a4f6ffb0138
Katelynconrad424 <katelynconrad424@gmail.com>
2026-04-09 21:32:19 -0400

    added data for restaurants

---
+++ src/data/restaurants.js
@@ -0,0 +1,49 @@
+const restaurants = [
+  {
+    id: 1,
+    name: "Taco House",
+    cuisine: "mexican",
+    price: "cheap",
+    vibe: "casual",
+    distance: "close",
+    dineType: "takeout",
+  },
+  {
+    id: 2,
+    name: "Olive Garden",
+    cuisine: "italian",
+    price: "medium",
+    vibe: "casual",
+    distance: "medium",
+    dineType: "dine-in",
+  },
+  {
+    id: 3,
+    name: "Sushi Spot",
+    cuisine: "japanese",
+    price: "expensive",
+    vibe: "fancy",
+    distance: "far",
+    dineType: "dine-in",
+  },
+  {
+    id: 4,
+    name: "Chick-fil-A",
+    cuisine: "american",
+    price: "cheap",
+    vibe: "fast",
+    distance: "close",
+    dineType: "takeout",
+  },
+  {
+    id: 5,
+    name: "Panera Bread",
+    cuisine: "healthy",
+    price: "medium",
+    vibe: "casual",
+    distance: "close",
+    dineType: "dine-in",
+  },
+];
+
+export default restaurants;
